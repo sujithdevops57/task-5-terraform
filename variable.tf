04294ed1cf9fd823d1b0fc3c5086a2d9808b3dd0
sujithdevops57 <sujithmanelli5657@gmail.com>
2025-11-13 13:04:41 +0530

    Add VM configuration and user name variables

---
+++ variable.tf
@@ -0,0 +1,29 @@
+variable "vm_config" {
+    type = list(object({
+      name = string
+      zone = string
+      machine_type = string
+      image = string
+    }))
+    default = [ {
+      name = "vm-1"
+      zone = "us-central1-a"
+      machine_type = "e2-medium"
+      image = "centos-cloud/centos-stream-9"
+    } ,{
+      name = "vm-2"
+      zone = "us-central1-b"
+      machine_type = "e2-medium"
+      image = "debian-cloud/debian-11" 
+    },
+   ]
+
+
+
+  
+}
+
+variable "user_name" {
+    type = string
+  default  = "sujithmanelli4321"
+}
